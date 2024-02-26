d26361968ec6d59b7fb4a892f7e704301f6d8fe9
236sec <popole159za@gmail.com>
2024-02-26 18:44:29 +0700

    first commit

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+    // Start is called before the first frame update
+
+    [ContextMenu("Inc Score")]
+    public void addScore(int scoreAdd)
+    {
+        Debug.Log("Adding score");
+        playerScore += scoreAdd;
+        scoreText.text = playerScore.ToString();
+    }
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
